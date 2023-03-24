de48c67031a181b49765763ec1cccb1872ca89dd
raph2ll <raphael.ph2ll@gmail.com>
2023-03-24 14:46:59 -0300

    feat: change base path

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import React from 'react';
+import Generate from './pages/Generate';
 
 function Router() {
   return (
     <Routes>
-      <Route path="/" element={<h1>Test</h1>} />
+      <Route path="/" element={<Generate />} />
     </Routes>
   );
 }
