532510103cf3465672a899fc6afbde96bb7e3c17
raph2ll <raphael.ph2ll@gmail.com>
2023-03-25 17:51:29 -0300

    feat: new route

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import React from 'react';
 import Generate from './pages/Generate';
+import About from './pages/About';
 
 function Router() {
   return (
     <Routes>
       <Route path="/" element={<Generate />} />
+      <Route path="/generate/:id" element={<About />} />
     </Routes>
   );
 }
