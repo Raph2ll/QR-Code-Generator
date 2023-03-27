aa56a0db8360c5964d398d496aca4c40200211bb
raph2ll <raphael.ph2ll@gmail.com>
2023-03-27 15:27:44 -0300

    feat: app qrSvg

--- backend/api/app.js
+++ backend/api/app.js
@@ -1,10 +1,20 @@
 const express = require('express');
-const router = require('../router');
+const qr = require('qr-image');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use('/', router);
+app.get('/qrcode', (req, res) => {
+  const { text } = req.query;
+  if (!text) {
+    return res.status(400).send({ error: 'No text provided' });
+  }
+  const qrSvg = qr.image(text, { type: 'svg' });
+  res.type('svg');
+  qrSvg.pipe(res);
+});
+
 
 module.exports = app;
